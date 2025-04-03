a9eb336491510c4088df868eccbcca22364ec96d
Yuliya Kudrautsava <y.kudrautsava@andersenlab.com>
2025-04-03 09:54:19 +0200

    fix: fixed config

--- innws-stack-overflow/vite.config.ts
+++ innws-stack-overflow/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Innws-Stack-Overflow/",
   plugins: [react()],
   server: {
     open: true,
